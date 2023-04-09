bc3e45f24670857874785b8f4719bac148a68e27
AlGhaliyahM <439201044@student.ksu.edu.sa>
2023-04-10 00:41:32 +0300

    refactor:changed isSupportedUrl method name to wishlistScraper which handels the validation and call scraper method based on the domain

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import { Scraper } from './scraper';
-import { WishlistItem } from './wishlist-item.interface';
-import { amazonSelector, mamasandpapasSelector, ounassSelector } from './enum';
 
 export { Scraper };
--- src/scraper.ts
+++ src/scraper.ts
@@ -10,110 +10,109 @@ export class Scraper {
   async mamasandpapasScraper(url: string): Promise<WishlistItem[] | ErrorMessage> {
     const wishlistItems: WishlistItem[] = [];
 
-    if (this.isSupportedUrl(url)) {
-      try {
-        // fetch html of the page we want to scrape
-        const { data } = await axios.get(url);
+    try {
+      // fetch html of the page we want to scrape
+      const { data } = await axios.get(url);
 
-        // Load html we fetched in the previous line
-        const $ = cheerio.load(data);
-        // select all the list wishlistItem in b-tab-content b-toggle__content m-expanded class
-        const list = $(mamasandpapasSelector.BASE_SELECTOR);
+      // Load html we fetched in the previous line
+      const $ = cheerio.load(data);
+      // select all the list wishlistItem in b-tab-content b-toggle__content m-expanded class
+      const list = $(mamasandpapasSelector.BASE_SELECTOR);
 
-        // use .each method to loop through the selected css
-        list.each((i, el) => {
-          wishlistItems.push({
-            item_name: $(el).find(mamasandpapasSelector.ITEM_NAME_SELECTOR).text(),
-            item_price: $(el).find(mamasandpapasSelector.ITEM_PRICE_SELECTOR).attr('content') + '',
-            item_img: $(el).find(mamasandpapasSelector.ITEM_IMG_SELECTOR).attr('src') + '',
-            item_url: $(el).find(mamasandpapasSelector.ITEM_URL_SELECTOR).attr('href') + '',
-          });
+      // use .each method to loop through the selected css
+      list.each((i, el) => {
+        wishlistItems.push({
+          item_name: $(el).find(mamasandpapasSelector.ITEM_NAME_SELECTOR).text(),
+          item_price: $(el).find(mamasandpapasSelector.ITEM_PRICE_SELECTOR).attr('content') + '',
+          item_img: $(el).find(mamasandpapasSelector.ITEM_IMG_SELECTOR).attr('src') + '',
+          item_url: $(el).find(mamasandpapasSelector.ITEM_URL_SELECTOR).attr('href') + '',
         });
+      });
 
-        return wishlistItems;
-      } catch (error) {
-        return setError('error encountred during fetching ');
-      }
-    } else {
-      return setError('URL is not valid');
+      return wishlistItems;
+    } catch (error) {
+      return setError('Error encountred during fetching ');
     }
   }
 
   async amazonScraper(url: string): Promise<WishlistItem[] | ErrorMessage> {
     const wishlistItems: WishlistItem[] = [];
 
-    if (this.isSupportedUrl(url)) {
-      try {
-        // fetch html of the page we want to scrape
-        const { data } = await axios.get(url);
+    try {
+      // fetch html of the page we want to scrape
+      const { data } = await axios.get(url);
 
-        // Load html we fetched in the previous line
-        const $ = cheerio.load(data);
+      // Load html we fetched in the previous line
+      const $ = cheerio.load(data);
 
-        const list = $(amazonSelector.BASE_SELECTOR);
+      const list = $(amazonSelector.BASE_SELECTOR);
 
-        // use .each method to loop through the selected css
-        list.each((i, el) => {
-          wishlistItems.push({
-            item_name: $(el).find(amazonSelector.ITEM_NAME_SELECTOR).attr('title') + '',
-            item_price: $(el).find(amazonSelector.ITEM_PRICE_SELECTOR).text(),
-            item_img: $(el).find(amazonSelector.ITEM_IMG_SELECTOR).attr('src') + '',
-            item_url: $(el).find(amazonSelector.ITEM_URL_SELECTOR).attr('href') + '',
-          });
+      // use .each method to loop through the selected css
+      list.each((i, el) => {
+        wishlistItems.push({
+          item_name: $(el).find(amazonSelector.ITEM_NAME_SELECTOR).attr('title') + '',
+          item_price: $(el).find(amazonSelector.ITEM_PRICE_SELECTOR).text(),
+          item_img: $(el).find(amazonSelector.ITEM_IMG_SELECTOR).attr('src') + '',
+          item_url: $(el).find(amazonSelector.ITEM_URL_SELECTOR).attr('href') + '',
         });
-        return wishlistItems;
-      } catch (error) {
-        return setError('error encountred during fetching ');
-      }
-    } else {
-      return setError('URL is not valid');
+      });
+      return wishlistItems;
+    } catch (error) {
+      return setError('Error encountred during fetching ');
     }
   }
 
   async ounassScraper(url: string): Promise<WishlistItem[] | ErrorMessage> {
     const wishlistItems: WishlistItem[] = [];
 
-    if (this.isSupportedUrl(url)) {
-      try {
-        // fetch html of the page we want to scrape
-        const { data } = await axios.get(url);
+    try {
+      // fetch html of the page we want to scrape
+      const { data } = await axios.get(url);
 
-        // Load html we fetched in the previous line
-        const $ = cheerio.load(data);
+      // Load html we fetched in the previous line
+      const $ = cheerio.load(data);
 
-        const list = $(ounassSelector.BASE_SELECTOR);
+      const list = $(ounassSelector.BASE_SELECTOR);
 
-        // use .each method to loop through the selected css
-        list.each((i, el) => {
-          wishlistItems.push({
-            item_name: $(el).find(ounassSelector.ITEM_NAME_SELECTOR).text(),
-            item_price: $(el).find(ounassSelector.ITEM_PRICE_SELECTOR).text(),
-            item_img: $(el).find(ounassSelector.ITEM_IMG_SELECTOR).attr('src') + '',
-            item_url: $(el).find(ounassSelector.ITEM_URL_SELECTOR).attr('href') + '',
-          });
+      // use .each method to loop through the selected css
+      list.each((i, el) => {
+        wishlistItems.push({
+          item_name: $(el).find(ounassSelector.ITEM_NAME_SELECTOR).text(),
+          item_price: $(el).find(ounassSelector.ITEM_PRICE_SELECTOR).text(),
+          item_img: $(el).find(ounassSelector.ITEM_IMG_SELECTOR).attr('src') + '',
+          item_url: $(el).find(ounassSelector.ITEM_URL_SELECTOR).attr('href') + '',
         });
+      });
 
-        return wishlistItems;
-      } catch (error) {
-        return setError('error encountred during fetching ');
-      }
-    } else {
-      return setError('URL is not valid');
+      return wishlistItems;
+    } catch (error) {
+      return setError('Error encountred during fetching ');
     }
   }
 
-  // validate url format method
-  isSupportedUrl(url: string) {
+  // validate url format method & call scraping method based on the domain
+  async wishlistScraper(url: string): Promise<WishlistItem[] | ErrorMessage | any> {
     try {
-      // TODO: validate if domain is supported.
       const supportedDomains = Object.values(supportedDomain);
       const wishlistUrl = new URL(url);
       let flag = false;
 
+      // check if the given domain url is supported
       for (const domain in supportedDomains) {
-        if (url.includes(domain)) flag = true;
+        if (url.includes(supportedDomains[domain]) && url.includes(supportedDomain.MAMAS_PAPAS_Domain)) {
+          flag = true;
+          return await this.mamasandpapasScraper(url);
+        }
+        if (url.includes(supportedDomains[domain]) && url.includes(supportedDomain.AMAZON_DOMAIN)) {
+          flag = true;
+          return await this.amazonScraper(url);
+        }
+        if (url.includes(supportedDomains[domain]) && url.includes(supportedDomain.OUNASS_DOMAIN)) {
+          flag = true;
+          return await this.ounassScraper(url);
+        }
       }
-      return Boolean(wishlistUrl) && flag;
+      if (flag == false) return setError('Domain not supported');
     } catch (e) {
       throw new Error('Invalid Url');
     }
