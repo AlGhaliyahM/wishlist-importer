f5bc455691df6f652bb9809a10eda703646239bf
AlGhaliyahM <439201044@student.ksu.edu.sa>
2023-04-10 23:22:08 +0300

    update test.ts

--- playground/test.ts
+++ playground/test.ts
@@ -7,11 +7,9 @@ async function test(url: string) {
   console.log(wishlist__product);
   return wishlist__product;
 }
-const urls = [
-  'https://en.mamasandpapas.com.sa/wishlist/shared?id=2d78c422df527b4a77bb283fb7',
-  'https://www.amazon.sa/-/en/hz/wishlist/ls/3HNOX1XHXP6X6?ref_=wl_fv_le&viewType=list',
-  'https://en-saudi.ounass.com/wishlist/shared/0cfa1b20-d32d-11ed-948c-db4d3a08f2f3',
-];
+
+//assign wishlist urls in urls[] array to test
+const urls = [''];
 const start = () => {
   urls.forEach((url) => {
     test(url);
